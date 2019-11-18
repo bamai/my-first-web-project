9b29bc5170f588c8544d20e2520971c0ce7a13f2
maib <maib@wix.com>
2019-11-18 13:52:11 +0200

    movieList with local database

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ let dataBase = [{"Title": "Good Will Hunting", "Year": "1997", "inDropdown":"no"
                 {"Title": "Alexander and the Terrible, Horrible, No Good, Very Bad Day", "Year": "2004", "inDropdown":"no"},
                 {"Title": "Midnight in the Garden of Good and Evil", "Year": "1997", "inDropdown":"no"},
                 {"Title": "The Good Son", "Year": "1993", "inDropdown":"no"},
-                {"Title": "Tow Is Good", "Year": "2012", "inDropdown":"no"},
-                {"Title": "Tjw Is Good", "Year": "2012", "inDropdown":"no"},
-                {"Title": "Ttw Is Good", "Year": "2012", "inDropdown":"no"},
-                {"Title": "Tow Is Good", "Year": "2012", "inDropdown":"no"}]
+                {"Title": "Now Is Good", "Year": "2012", "inDropdown":"no"}]
 
 window.onload = function(){
     createMovieList(document.getElementById('mv1'));
@@ -28,30 +25,78 @@ window.onload = function(){
 
 function createMovieList(element) {
     const list = element.getElementsByClassName("list")[0];
+    console.log(list.classList);
     const menu = element.getElementsByClassName("options")[0];
     const addButton = element.getElementsByClassName("add")[0];
     const input = element.getElementsByClassName("input")[0];
     const dropdown = element.getElementsByClassName("dropdown")[0];
     let movies = [];
 
+    let createLi = function(movie){
+        let todoButton = document.createElement('BUTTON');
+        todoButton.appendChild(document.createElement('IMG'));
+        todoButton.firstChild.src = "unticked.png";
+        todoButton.className = "isviewed";
+        let rmvButton = document.createElement('BUTTON');
+        rmvButton.appendChild(document.createElement('IMG'));
+        rmvButton.firstChild.src = "remove.png";
+        rmvButton.className = "remove";
+        let span = document.createElement('SPAN');
+        span.appendChild(document.createTextNode(movie));
+        let li = document.createElement('LI');
+        li.appendChild(todoButton);
+        li.appendChild(span);
+        li.appendChild(rmvButton);
+        li.className = "unviewed";
+        return li;
+    }
+
+    let addMovie = function(movie){
+        for(let i=0;i<movies.length;i++){
+            if(movies[i].toLowerCase()===movie.toLowerCase()){
+                alert("you already have this movie");
+                return false;
+            }
+        }
+        movies.push(movie);
+        list.appendChild(createLi(movie));
+        return true;
+    }
+
+    let addHandler = function(){
+        if(!(input.value=== ""||input.value == null)){
+            if(list.children[0].textContent === "you have no movies"){
+                list.removeChild(list.children[0]);
+            }
+            if(addMovie(input.value)){
+                input.value = "";
+            }
+        }
+        else{
+            alert("no movie to add");
+        }
+    }
+
+    let setLiStatus = function(li,finalClass){
+        li.style.opacity = "0";
+        setTimeout(()=>{li.className = finalClass;
+                        li.style.opacity = "1";},400);
+    }
+    
     let clickHandler = function(event){
         let button = event.target.parentNode;
         if(button.className === "isviewed"){
             event.target.src = event.target.src.endsWith("unticked.png")? "ticked.jpeg" : "unticked.png";
             if(button.parentNode.className === "unviewed"){
                 if(list.className === "listUnviewed"){
-                    button.parentNode.style.opacity = "0";
-                    setTimeout(()=>{button.parentNode.className = "viewed";
-                                    button.parentNode.style.opacity = "1";},400)
+                    setLiStatus(button.parentNode,"viewed");
                 }
                 else
                     button.parentNode.className = "viewed";
             }
             else{
                 if(list.className === "listViewed"){
-                    button.parentNode.style.opacity = "0";
-                    setTimeout(()=>{button.parentNode.className = "unviewed";
-                                    button.parentNode.style.opacity = "1";},400)
+                    setLiStatus(button.parentNode,"unviewed")
                 }
                 else
                     button.parentNode.className = "viewed";
@@ -75,46 +120,11 @@ function createMovieList(element) {
                         li.appendChild(document.createTextNode("you have no movies"));
                         list.appendChild(li);}},400);
         }
-    }
-    
-    let createLi = function(){
-        let todoButton = document.createElement('BUTTON');
-        todoButton.appendChild(document.createElement('IMG'));
-        todoButton.firstChild.src = "unticked.png";
-        todoButton.className = "isviewed";
-        let rmvButton = document.createElement('BUTTON');
-        rmvButton.appendChild(document.createElement('IMG'));
-        rmvButton.firstChild.src = "remove.png";
-        rmvButton.className = "remove";
-        let span = document.createElement('SPAN');
-        span.appendChild(document.createTextNode(input.value));
-        let li = document.createElement('LI');
-        li.appendChild(todoButton);
-        li.appendChild(span);
-        li.appendChild(rmvButton);
-        li.className = "unviewed";
-        return li;
+        if(button.className === "dropdownLi"){
+            addMovie(event.target.textContent);
+        }
     }
 
-    let addHandler = function(){
-        if(!(input.value=== ""||input.value == null)){
-            if(list.children[0].textContent === "you have no movies"){
-                list.removeChild(list.children[0]);
-            }
-            for(let i=0;i<movies.length;i++){
-                if(movies[i]===input.value){
-                    alert("you already have this movie");
-                    return;
-                }
-            }
-            movies.push(input.value);
-            list.appendChild(createLi());
-            input.value = "";
-        }
-        else{
-            alert("no movie to add");
-        }
-    }
 
     let changeListMode = function(mode){
         if(mode === "viewed"){
@@ -171,7 +181,7 @@ function createMovieList(element) {
     let deleteFromDropdown = function(movie){
         for(let i=0;i<dropdown.children.length;i++){
             let title = dropdown.children[i].children[0].textContent;
-            title = title.slice(0,title.indexOf(','));
+            title = title.slice(0,title.lastIndexOf(','));
             if(title === movie.Title){
                 dropdown.removeChild(dropdown.children[i]);
             }
@@ -180,18 +190,23 @@ function createMovieList(element) {
     let inputHandler = function(){
         if(input.value!==""&&input.value!=null){
             for(let i=0; i<dataBase.length;i++){
-                console.log(input.value);
-                if(dataBase[i].Title.startsWith(input.value)&&dataBase[i].inDropdown==="no"){
+                let name = dataBase[i].Title.toLowerCase()+", "+dataBase[i].Year;
+                if(name.startsWith(input.value.toLowerCase())&&dataBase[i].inDropdown==="no"){
                     dropdown.appendChild(createDropdwonli(dataBase[i]));
                     dataBase[i].inDropdown = "yes";
+                    dropdown.style.display = "block";
                 }
-                if(!(dataBase[i].Title.startsWith(input.value))&&dataBase[i].inDropdown==="yes"){
+                if(!(name.startsWith(input.value.toLowerCase()))&&dataBase[i].inDropdown==="yes"){
                     deleteFromDropdown(dataBase[i]);
                     dataBase[i].inDropdown = "no";
                 }
             }
+            if(dropdown.children.length===0){
+                dropdown.style.display = "none";
+            }
         }
         else{
+            dropdown.style.display = "none";
             while(dropdown.firstChild){
                 dropdown.removeChild(dropdown.firstChild);
             }
@@ -206,4 +221,5 @@ function createMovieList(element) {
     addButton.addEventListener('click', addHandler);
     menu.addEventListener('click',menuHandler);
     input.addEventListener('input',inputHandler);
+    dropdown.addEventListener('click',clickHandler);
 }
